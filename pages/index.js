658c13f14b7098f186d3e6cfdd6f9ed8481c66ed
karkoszkaondrej <karkoszkaondrej@gmail.com>
2022-05-19 15:07:25 +0200

    Assets, Metadata, and CSS lesson end

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,13 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>
+          Hello, I'm Ondřej. I'm software developer in Kotlin, C# and
+          Typescript. You can contact me on{' '}
+          <a href="https://www.linkedin.com/in/ond%C5%99ej-karkoszka-421077196">
+            LinkedIn
+          </a>
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
